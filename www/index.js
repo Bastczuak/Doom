ec34e856da1a018f712a2e891ce0788798218602
Sebastian Richter <sebastian.richter@paessler.com>
2019-11-01 23:40:05 +0100

    -Added three js for rendering.

--- www/index.js
+++ www/index.js
@@ -1,7 +1,29 @@
 import * as doom from 'doom'
+import * as THREE from 'three'
 
-fetch('./Doom1.wad').then(async response => {
+(async function run () {
+  const response = await fetch('./Doom1.wad')
   const downloadedMap = await response.arrayBuffer()
   const map = doom.init(downloadedMap)
   console.log('###', map)
-})
+
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  camera.position.z = 5
+
+  const animate = () => {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+  }
+  animate();
+})()
