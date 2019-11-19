2137b1a28aa11af22a7945968b0a1718c7a5ad4a
Sebastian Richter <sebastian.richter@paessler.com>
2019-11-19 13:49:17 +0100

    -Added resizer of the canvas.

--- www/index.js
+++ www/index.js
@@ -106,6 +106,12 @@ function addNodes ({ nodes, xShift, yShift }, scene) {
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
 
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }, false)
+
   const animate = () => {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
